2cb8a12eacbd91a743c5abd884f6da737602b9c7
Guillaume Yvon <neoklaoude@gmail.com>
2022-12-09 12:23:43 +0100

    Fix configuration update

--- QuickerStackPlugin.cs
+++ QuickerStackPlugin.cs
@@ -26,15 +26,14 @@ namespace QuickerStack
             {
                 QuickerStackPlugin.configFile = new ConfigFile(this.ConfigPath, true);
                 this.LoadConfig();
-                QuickerStackPlugin.configFile = base.Config;
-                File.Delete(this.ConfigPath);
-                this.LoadConfig();
             }
             else
             {
                 QuickerStackPlugin.configFile = base.Config;
                 this.LoadConfig();
             }
+
+            Config.SettingChanged += OnSettingChanged;
 
             Harmony.CreateAndPatchAll(Assembly.GetExecutingAssembly(), null);
         }
@@ -70,7 +69,12 @@ namespace QuickerStack
             QuickerStackPlugin.NearbyRange = this.BindParameter<float>(QuickerStackPlugin.NearbyRange, "NearbyRange", "How far from you is nearby, greater value = greater range");
             QuickerStackPlugin.IgnoreConsumable = this.BindParameter<bool>(QuickerStackPlugin.IgnoreConsumable, "IgnoreConsumable", "Whether to completely exclude consumables from quick stacking (food, potions).");
             QuickerStackPlugin.IgnoreAmmo = this.BindParameter<bool>(QuickerStackPlugin.IgnoreAmmo, "IgnoreAmmo", "Whether to completely exclude ammo from quick stacking (arrows)");
-            QuickerStackPlugin.CoalesceTrophies = this.BindParameter<bool>(QuickerStackPlugin.CoalesceTrophies, "CoalesceTrophies", "Whether to put all types of trophies in the container if any trophy is found in that container.");
+            QuickerStackPlugin.CoalesceTrophies = this.BindParameter<bool>(QuickerStackPlugin.CoalesceTrophies, "CoalesceTrophies", "Whether to put all types of trophies in the container if any trophy is found in that container."); 
+        }
+
+        private void OnSettingChanged(object sender, SettingChangedEventArgs e)
+        {
+            LoadConfig();
         }
 
         public static List<Container> FindContainersInRange(Vector3 point, float range)
@@ -208,9 +212,10 @@ namespace QuickerStack
         public static List<Container> AllContainers = new List<Container>();
         private readonly string ConfigPath = Path.Combine(Paths.ConfigPath, "QuickerStack.cfg");
         private static ConfigFile configFile = null;
+        public event EventHandler<SettingChangedEventArgs> SettingChanged;
         public static bool IgnoreAmmo = true;
         public static bool IgnoreConsumable = true;
-        internal static float NearbyRange = 150f;
+        internal static float NearbyRange = 15f;
         public static KeyCode QuickStackKey = KeyCode.P;
         public static bool CoalesceTrophies = true;
         private static Dictionary<long, UserConfig> playerConfigs = new Dictionary<long, UserConfig>();
