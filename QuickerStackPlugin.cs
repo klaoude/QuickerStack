2dd6051ce5c58962c72f641b55022c6642b786ba
Guillaume Yvon <neoklaoude@gmail.com>
2022-12-09 13:52:50 +0100

    change version

--- QuickerStackPlugin.cs
+++ QuickerStackPlugin.cs
@@ -16,7 +16,7 @@ namespace QuickerStack
     [BepInPlugin("org.bepinex.plugins.valheim.quicker_stack", "Quicker Stack", VERSION)]
     public class QuickerStackPlugin : BaseUnityPlugin
     {
-        private const string VERSION = "0.0.2";
+        private const string VERSION = "0.0.3";
 
         public void Awake()
         {
